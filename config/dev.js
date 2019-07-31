0002b7aebec5bcb2e1004ca9316c7023474e65eb
MedinaVilla <net_medina@hotmail.com>
2019-07-31 13:19:00 -0500

    Blogster

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://blogster:blogster1@ds349857.mlab.com:49857/blogster',
+  cookieKey: '123123123'
+};
